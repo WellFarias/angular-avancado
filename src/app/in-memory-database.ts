9e9e89c2e567787c5537e88625d99a03fdd66a8a
WellFarias <well.farias1969@gmail.com>
2021-07-21 16:18:35 -0300

    created category.model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,15 +1,16 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Category } from './pages/categories/shared/category.model'
 
 export class InMemoryDatabase implements InMemoryDbService {
     createDb() {
-        const categories: Array<Object> = [
+        const categories: Array<Category> = [
             { id: 1, name: 'Moradia', description: 'Pagamentos de Contas da Casa' },
             { id: 2, name: 'Saúde', description: 'Planos de Saúde e Remédios' },
             { id: 3, name: 'Lazer', description: 'Cinema, Parque, Praia, etc..' },
             { id: 4, name: 'Salário', description: 'Recebimento de Salário' },
             { id: 5, name: 'Freelas', description: 'Trabalhos com Freelancer' }
         ]
-        
+
         return { categories }
     }
 }
